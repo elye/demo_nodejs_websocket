32b7eec6a3ba7166dd31086a9e42237ede091e6d
elye <elye.project@gmail.com>
2021-10-24 19:44:30 +1100

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8082 });
+
+wss.getUniqueID = function () {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4();
+};
+
+wss.on("connection", ws => {
+    ws.id = wss.getUniqueID();
+    console.log(`New client connected with id: ${ws.id}`);
+
+    ws.on("message", data => {
+        console.log(`Client ${ws.id}: ${data}`);
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(`${data}`);
+            }
+        });
+    });
+
+    ws.on("close", () => {
+        console.log(`Client ${ws.id} has disconnected!`);
+    });
+});
